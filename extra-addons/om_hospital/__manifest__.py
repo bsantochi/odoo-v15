fdce3d786ab81c938e1badaedaa3ae217d088c36
bsantochi <santochibelen@gmail.com>
2023-06-06 15:17:18 -0300

    ejercicio guia de desarrollo

---
+++ extra-addons/om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'author': 'Beitech',
+    'summary': 'Hospital management system',
+    'description': """
+    Hospital management system
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'aplication': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'assets': {},
+    
+}
