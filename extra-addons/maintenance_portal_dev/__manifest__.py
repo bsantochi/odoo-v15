f6e81c7b794f076237f1808421d75eb7122b0fe0
bsantochi <santochibelen@gmail.com>
2023-06-08 16:28:16 -0300

    add extra modules

--- extra-addons/maintenance_portal_dev/__manifest__.py
+++ extra-addons/maintenance_portal_dev/__manifest__.py
@@ -3,20 +3,19 @@
     'name': "maintenance_portal_dev",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo CRM para la gestión de visitas""",
 
     'description': """
-        Long description of module's purpose
+        Modulo CRM para la gestión de visitas...
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Beitech",
+    'website': "http://www.beitechcom",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Maintenance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,4 +31,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': False
 }
